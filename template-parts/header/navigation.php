634c4a43aa6720ef528deb2c069cbc25cbe7b071
Morgan Segura <morgan@jointhebibleproject.com>
2022-06-03 17:38:32 -0700

    Gets menus back

--- template-parts/header/navigation.php
+++ template-parts/header/navigation.php
@@ -14,77 +14,83 @@ namespace Few_Crew;
 
 ?>
 
-<div class="mainnav">
+<nav id="nav" class="header-nav">
 
-  <!-- NB! Accessibility: Add/remove has-visible-label class for button if you want to enable/disable visible "Show menu/Hide menu" label for seeing users -->
-  <!-- <button aria-controls="nav" id="nav-toggle" class="nav-toggle hamburger has-visible-label" type="button"
-    aria-label="<?php echo esc_html( get_default_localization( 'Open main menu' ) ); ?>">
-    <span class="hamburger-box">
-      <span class="hamburger-inner"></span>
-    </span>
-    <span id="nav-toggle-label"
-      class="nav-toggle-label"><?php echo esc_html( get_default_localization( 'Open main menu' ) ); ?></span>
-  </button> -->
+  <div class="pushy-panel__toggle mobile">
+    <span class="pushy-panel__line"></span>
+  </div>
 
-  <nav id="nav" class="header-nav">
-    <div class="header-menu header-menu-admin"
-      aria-label="<?php echo esc_html( get_default_localization( 'Main navigation' ) ); ?>">
-      <div class="contain-xxl">
-        <div class="contain-xl">
-          <?php wp_nav_menu( array(
-                'theme_location' => 'header_primary',
-                'container'      => false,
-                'depth'          => 4,
-                'menu_class'     => 'header-menu-items',
-                'menu_id'        => 'header_primary',
-                'echo'           => true,
-                'fallback_cb'    => __NAMESPACE__ . '\Nav_Walker::fallback',
-                'items_wrap'     => '<div class="header-menu-admin"><ul id="%1$s" class="%2$s">%3$s</ul></div>',
-                'has_dropdown'   => true,
-                'walker'         => new Nav_Walker(),
-              ) ); ?>
+  <div class="header-nav-control">
+
+      <div class="pushy-panel__back-btn-block pushy-panel--dark">
+        <div class="pushy-panel__back-btn"></div>
+      </div>
+
+      <div class="header-menu header-menu-admin"
+        aria-label="<?php echo esc_html( get_default_localization( 'Main navigation' ) ); ?>">
+        <div class="contain-xxl">
+          <div class="contain-xl">
+            <?php wp_nav_menu( array(
+                    'theme_location' => 'header_primary',
+                    'container'      => false,
+                    'depth'          => 4,
+                    'menu_class'     => 'header-menu-items',
+                    'menu_id'        => 'header_primary',
+                    'echo'           => true,
+                    'fallback_cb'    => __NAMESPACE__ . '\Nav_Walker::fallback',
+                    'items_wrap'     => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+                    'has_dropdown'   => true,
+                    'walker'         => new Nav_Walker(),
+                  ) ); ?>
+          </div>
         </div>
       </div>
-    </div>
 
-    <div class="header-menu header-menu-ads"
-      aria-label="<?php echo esc_html( get_default_localization( 'Main navigation' ) ); ?>">
-      <div class="contain-xxl">
-        <div class="contain-xl">
-          <?php wp_nav_menu( array(
-                'theme_location' => 'header_secondary',
-                'container'      => false,
-                'depth'          => 4,
-                'menu_class'     => 'header-menu-items',
-                'menu_id'        => 'header_secondary',
-                'echo'           => true,
-                'fallback_cb'    => __NAMESPACE__ . '\Nav_Walker::fallback',
-                'items_wrap'     => '<div class="header-menu-ads"><ul id="%1$s" class="%2$s">%3$s</ul></div>',
-                'has_dropdown'   => true,
-                'walker'         => new Nav_Walker(),
-              ) ); ?>
+      <div class="header-menu header-menu-ads"
+        aria-label="<?php echo esc_html( get_default_localization( 'Main navigation' ) ); ?>">
+        <div class="contain-xxl">
+          <div class="contain-xl">
+            <?php wp_nav_menu( array(
+                    'theme_location' => 'header_secondary',
+                    'container'      => false,
+                    'depth'          => 4,
+                    'menu_class'     => 'header-menu-items',
+                    'menu_id'        => 'header_secondary',
+                    'echo'           => true,
+                    'fallback_cb'    => __NAMESPACE__ . '\Nav_Walker::fallback',
+                    'items_wrap'     => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+                    'has_dropdown'   => true,
+                    'walker'         => new Nav_Walker(),
+                  ) ); ?>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="header-menu header-menu-pages"
-      aria-label="<?php echo esc_html( get_default_localization( 'Main navigation' ) ); ?>">
-      <div class="contain-xxl">
-        <div class="contain-xl">
-          <?php wp_nav_menu( array(
-                'theme_location' => 'header_tertiary',
-                'container'      => false,
-                'depth'          => 4,
-                'menu_class'     => 'headernav-pagemenu menu-items',
-                'menu_id'        => 'header_tertiary',
-                'echo'           => true,
-                'fallback_cb'    => __NAMESPACE__ . '\Nav_Walker::fallback',
-                'items_wrap'     => '<div class="header-menu-page"><ul id="%1$s" class="%2$s">%3$s</ul></div>',
-                'has_dropdown'   => true,
-                'walker'         => new Nav_Walker(),
-              ) ); ?>
+
+      <div class="header-menu header-menu-page"
+        aria-label="<?php echo esc_html( get_default_localization( 'Main navigation' ) ); ?>">
+        <div class="contain-xxl">
+          <div class="contain-xl">
+              <div class="header-menu-items">
+              <?php wp_nav_menu( array(
+                      'theme_location' => 'header_tertiary',
+                      'container'      => false,
+                      'depth'          => 4,
+                      'menu_class'     => 'header-menu-items',
+                      'menu_id'        => 'header_tertiary',
+                      'echo'           => true,
+                      'fallback_cb'    => __NAMESPACE__ . '\Nav_Walker::fallback',
+                      'items_wrap'     => '<ul id="%1$s" class="%2$s"><li>Morgan</li> %3$s</ul>',
+                      'has_dropdown'   => true,
+                      'walker'         => new Nav_Walker(),
+                    ) ); ?>
+                    <div class="pushy-panel__toggle desktop">
+                      <span class="pushy-panel__line"></span>
+                    </div>
+              </div>
+          </div>
         </div>
       </div>
-    </div>
+  </div>
 
-  </nav><!-- #nav -->
-</div>
+</nav><!-- #nav -->
+
