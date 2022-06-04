57400a22d57f36fc741951504d6a412b0c6c8f14
Morgan Segura <morgan@jointhebibleproject.com>
2022-06-03 23:09:12 -0700

    Updates mobile nav

--- template-parts/header/branding.php
+++ template-parts/header/branding.php
@@ -12,19 +12,17 @@ $description = get_bloginfo( 'description', 'display' );
 
 <div class="header-branding">
 
-  <div class="contain-xl">
-    <p class="header-title">
-      <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-        <span class="screen-reader-text"><?php bloginfo( 'name' ); ?></span>
-        <?php include get_theme_file_path( THEME_SETTINGS['logo'] ); ?>
-      </a>
-    </p>
-
-    <?php if ( $description || is_customize_preview() ) : ?>
-    <p class="header-description screen-reader-text">
-      <?php echo esc_html( $description ); ?>
-    </p>
-    <?php endif; ?>
+  <div class="header-title">
+    <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+      <span class="screen-reader-text"><?php bloginfo( 'name' ); ?></span>
+      <?php include get_theme_file_path( THEME_SETTINGS['logo'] ); ?>
+    </a>
   </div>
+
+  <?php if ( $description || is_customize_preview() ) : ?>
+  <p class="header-description screen-reader-text">
+    <?php echo esc_html( $description ); ?>
+  </p>
+  <?php endif; ?>
 
 </div>
