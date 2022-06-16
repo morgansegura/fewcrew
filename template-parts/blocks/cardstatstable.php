1c8d2362d9f7cdc63f87d980b8da25a452521255
Morgan Segura <morgan@jointhebibleproject.com>
2022-06-15 17:48:48 -0700

    Updates table

--- template-parts/blocks/cardstatstable.php
+++ template-parts/blocks/cardstatstable.php
@@ -38,35 +38,141 @@ if( have_rows('cardstatstable') ):
 endif;
 ?>
 
-<div class="card-statstable">
-  <div class="card-statstable-moreblock">
-    <h4 class="card-statstable-moreblock-title">Playoff Standings</h4>
-    <a href="" title="" class="card-statstable-moreblock-button">
-      See All Stats
-    </a>
-  </div>
-  <div class="card-statstable-table">
-    <div class="card-statstable-tablehead">
-      <div class="card-statstable-tablehead-col">Team Positions</div>
-      <div class="card-statstable-tablehead-col">W</div>
-      <div class="card-statstable-tablehead-col">L</div>
-      <div class="card-statstable-tablehead-col">GB</div>
-    </div>
-    <div class="card-statstable-gribody">
-      <!-- Column 01 -->
-      <div class="card-statstable-tablebody-col">
-        <div class="card-statstable-table-number">01</div>
-        <figure class="card-statstable-table-icon">
-          <img src="https://alchemists.dan-fisher.dev/basketball/assets/images/samples/logos/pirates_shield.png"
-            alt="L.A Pirates" />
-        </figure>
-        <div class="card-statstable-tablebody-col-heading">
-          <div class="card-statstable-table-title">LA Prates</div>
-          <div class="card-statstable-table-subtitle">Beebop Institute</div>
+<div class="contain-xxl">
+    <div class="contain-xl">
+        <div class="card-statstable">
+            <div class="card-statstable-moreblock">
+                <h4 class="card-statstable-moreblock-title">Schedule</h4>
+                <a href="" title="" class="card-statstable-moreblock-button">
+                    See All Stats
+                </a>
+            </div>
+            <div class="card-statstable-table">
+                <div class="card-statstable-tablehead">
+                    <div class="card-statstable-tablerow">
+                        <div class="card-statstable-table-col-lg">Team Positions</div>
+                        <div class="card-statstable-grid">
+                            <div class="card-statstable-table-col">W</div>
+                            <div class="card-statstable-table-col">L</div>
+                            <div class="card-statstable-table-col">GB</div>
+                            <div class="card-statstable-table-col">PTS</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card-statstable-tablebody">
+                <!-- Column 01 -->
+                <div class="card-statstable-tablerow">
+                    <div class="card-statstable-table-col-lg">
+                        <div class="card-statstable-table-number">01</div>
+                        <figure class="card-statstable-table-icon">
+                            <img src="https://alchemists.dan-fisher.dev/basketball/assets/images/samples/logos/pirates_shield.png"
+                                alt="L.A Pirates" />
+                        </figure>
+                        <div>
+                            <div class="card-statstable-table-col-heading">
+                                <div class="card-statstable-table-title">LA Prates</div>
+                                <div class="card-statstable-table-subtitle">Beebop Institute</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-statstable-grid">
+                        <div class="card-statstable-table-col">23</div>
+                        <div class="card-statstable-table-col">12</div>
+                        <div class="card-statstable-table-col">GB</div>
+                        <div class="card-statstable-table-col">PTS</div>
+                    </div>
+                </div>
+                <!-- Column 01 -->
+                <div class="card-statstable-tablerow">
+                    <div class="card-statstable-table-col-lg">
+                        <div class="card-statstable-table-number">01</div>
+                        <figure class="card-statstable-table-icon">
+                            <img src="https://alchemists.dan-fisher.dev/basketball/assets/images/samples/logos/pirates_shield.png"
+                                alt="L.A Pirates" />
+                        </figure>
+                        <div>
+                            <div class="card-statstable-table-col-heading">
+                                <div class="card-statstable-table-title">LA Prates</div>
+                                <div class="card-statstable-table-subtitle">Beebop Institute</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-statstable-grid">
+                        <div class="card-statstable-table-col">W</div>
+                        <div class="card-statstable-table-col">L</div>
+                        <div class="card-statstable-table-col">GB</div>
+                        <div class="card-statstable-table-col">PTS</div>
+                    </div>
+                </div>
+                <!-- Column 01 -->
+                <div class="card-statstable-tablerow">
+                    <div class="card-statstable-table-col-lg">
+                        <div class="card-statstable-table-number">01</div>
+                        <figure class="card-statstable-table-icon">
+                            <img src="https://alchemists.dan-fisher.dev/basketball/assets/images/samples/logos/pirates_shield.png"
+                                alt="L.A Pirates" />
+                        </figure>
+                        <div>
+                            <div class="card-statstable-table-col-heading">
+                                <div class="card-statstable-table-title">LA Prates</div>
+                                <div class="card-statstable-table-subtitle">Beebop Institute</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-statstable-grid">
+                        <div class="card-statstable-table-col">W</div>
+                        <div class="card-statstable-table-col">L</div>
+                        <div class="card-statstable-table-col">GB</div>
+                        <div class="card-statstable-table-col">PTS</div>
+                    </div>
+                </div>
+                <!-- Column 01 -->
+                <div class="card-statstable-tablerow">
+                    <div class="card-statstable-table-col-lg">
+                        <div class="card-statstable-table-number">01</div>
+                        <figure class="card-statstable-table-icon">
+                            <img src="https://alchemists.dan-fisher.dev/basketball/assets/images/samples/logos/pirates_shield.png"
+                                alt="L.A Pirates" />
+                        </figure>
+                        <div>
+                            <div class="card-statstable-table-col-heading">
+                                <div class="card-statstable-table-title">LA Prates</div>
+                                <div class="card-statstable-table-subtitle">Beebop Institute</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-statstable-grid">
+                        <div class="card-statstable-table-col">W</div>
+                        <div class="card-statstable-table-col">L</div>
+                        <div class="card-statstable-table-col">GB</div>
+                        <div class="card-statstable-table-col">PTS</div>
+                    </div>
+                </div>
+                <!-- Column 01 -->
+                <div class="card-statstable-tablerow">
+                    <div class="card-statstable-table-col-lg">
+                        <div class="card-statstable-table-number">01</div>
+                        <figure class="card-statstable-table-icon">
+                            <img src="https://alchemists.dan-fisher.dev/basketball/assets/images/samples/logos/pirates_shield.png"
+                                alt="L.A Pirates" />
+                        </figure>
+                        <div>
+                            <div class="card-statstable-table-col-heading">
+                                <div class="card-statstable-table-title">LA Prates</div>
+                                <div class="card-statstable-table-subtitle">Beebop Institute</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-statstable-grid">
+                        <div class="card-statstable-table-col">W</div>
+                        <div class="card-statstable-table-col">L</div>
+                        <div class="card-statstable-table-col">GB</div>
+                        <div class="card-statstable-table-col">PTS</div>
+                    </div>
+                </div>
+
+            </div>
         </div>
-      </div>
-
-
     </div>
-  </div>
 </div>
