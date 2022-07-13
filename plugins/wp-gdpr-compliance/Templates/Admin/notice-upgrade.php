0be11261d8ecd1e3e68953d75b956e23c197f9a8
Morgan Segura <morgan@jointhebibleproject.com>
2022-07-13 15:48:03 -0700

    Fewcrew-wp

--- plugins/wp-gdpr-compliance/Templates/Admin/notice-upgrade.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-use WPGDPRC\Utils\Elements;
-
-Elements::notice(__("It's the right time to get a comploant cookie banner for your website - FREE for one year", 'van-ons'), \WPGDPRC\Utils\Template::get('Admin/notice-upgrade-content'), '', 'upgrade' );
